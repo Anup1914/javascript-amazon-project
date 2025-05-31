225596a6606576efe339e0a0003e74176b91fb9f
Anup <anup27062001@gmail.com>
2025-05-31 11:06:30 +0530

    delivery days updation in checkout page

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -73,7 +73,6 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () =>{
     const productId = button.dataset.productId;
     addToCart(productId);
-
     updateCartQuantity();
   });
 });
