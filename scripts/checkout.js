b7f0b6f88565e2778c9285b02df1fd6afb185daf
Anup <anup27062001@gmail.com>
2025-06-04 15:34:09 +0530

    learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,58 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
-loadProducts(() => {
+Promise.all([
+new Promise((resolve) => {
+//it runs the inner function immediately
+  loadProducts(() => {
+    resolve();
+  });
+}),
+new Promise((resolve) => {
+  loadCart(() => {
+    resolve();
+  });
+})
+]).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+//it runs the inner function immediately
+  loadProducts(() => {
+    resolve();
+  });
+
+}).then(() => {
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+
+  }).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+  });
+})
+//resolve is function - let us control when to go to the next step
+// multiple callbacks cause nesting and that's why use promise
+/*
+loadProducts(() => {
+   renderOrderSummary();
+  renderPaymentSummary();
+});
+*/
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+*/
